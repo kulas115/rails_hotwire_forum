b25e7c4f428eeffea93c809988acb0b1eaf79122
kulas115 <kulas115@gmail.com>
2022-05-05 16:11:07 +0200

    Create discussions from the UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root to: 'main#index'
-  resources :discussions, only: [:index]
+  resources :discussions, only: %i[index create new]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
