e6551f70596909ded0bbb8eb6e5e68a23b5397e2
kulas115 <kulas115@gmail.com>
2022-05-08 12:08:24 +0200

    Add edit with Turbo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root to: 'main#index'
-  resources :discussions, only: %i[index create new edit update destroy]
+  resources :discussions
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
