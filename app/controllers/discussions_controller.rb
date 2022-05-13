a79eb8475f323069563b915c501058ae80804dd3
kulas115 <kulas115@gmail.com>
2022-05-13 08:49:44 +0200

    Add new posts with discussion

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -12,6 +12,7 @@ class DiscussionsController < ApplicationController
 
   def new
     @discussion = Discussion.new
+    @discussion.posts.new
   end
 
   def create
@@ -51,6 +52,6 @@ class DiscussionsController < ApplicationController
   end
 
   def discussion_params
-    params.require(:discussion).permit(:name, :closed, :pinned)
+    params.require(:discussion).permit(:name, :closed, :pinned, posts_attributes: :body)
   end
 end
